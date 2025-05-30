7422b61527dbc8c0b775c3800e8825aa7e2e3b95
AHAMED151542 <xlith25@gmail.com>
2025-05-30 09:11:47 -0700

    Update script.js

--- upload-ayop/assets/js/script.js
+++ upload-ayop/assets/js/script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "1": { // الصف الأول
             "math": {
                 "1": [
-                    { type: "مذكرة", name: "مذكرة الرياضيات الفصل الأول", date: "2023-09-15", link: "https://cdn.discordapp.com/attachments/1335632201244479580/1377336638732107836/a499b6c97318ef94ded5d4fc39e81a22.pdf?ex=68389834&is=683746b4&hm=b33618915273c97b7da10f60d7cbc7a431a90b57748b7cac33ee78ea3c2b345b&" },
+                    { type: "مذكرة", name: "مذكرة الرياضيات الفصل الأول", date: "2023-09-15", link: "https://newmoesitev2.blob.core.windows.net/files/moelibrarymedia/02061914-cc5f-477e-a4ce-12c8a90abf14.pdf" },
                     { type: "اختبار", name: "اختبار الفصل الأول", date: "2023-10-20", link: "#" }
                 ],
                 "2": [
@@ -94,3 +94,17 @@ function updateUI(isDark) {
 
   }
 }
+
+
+
+document.getElementById("studentbuttoninindex").addEventListener("click", function () {
+  window.location.href = "students.html"; // استبدل باسم صفحتك
+});
+
+document.getElementById("aboutbuttoninindex").addEventListener("click", function () {
+  window.location.href = "about.html"; // استبدل باسم صفحتك
+});
+
+document.getElementById("contactwithusonindex").addEventListener("click", function () {
+  window.location.href = "contact.html"; // استبدل باسم صفحتك
+});
